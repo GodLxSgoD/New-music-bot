af39f288cdec614ddacb7a3d2f044221dd6041b4
GodLxSgoD <165937925+GodLxSgoD@users.noreply.github.com>
2026-08-06 01:42:50 +0530

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -4,7 +4,7 @@ import discord
 from discord.ext import commands
 import yt_dlp as youtube_dl
 
-BOT_TOKEN = os.environ.get("MTUzNDY0ODgzMDU3MjYyNTk0MA.G8Chw2.ZUeRKqN3f0y3xd0QXAVC2mNcGaqenrPkY9dY34")
+BOT_TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
 COMMAND_PREFIX = "!"
 
 if not BOT_TOKEN:
